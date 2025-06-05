5d44ff3962071f698c010624b1188f46f401b5dc
andrehdsc <andrehdsc@gmail.com>
2025-06-05 16:21:17 -0300

    HOE-64

---
+++ 160-HOE-64/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	got := add(7, 5)
+	want := 12
+	if got != want {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", got, want)
+	}
+}
+func TestSubtract(t *testing.T) {
+	got := subtract(17, 5)
+	want := 12
+	if got != want {
+		t.Errorf("Subtract was incorrect, got: %d, want: %d.", got, want)
+	}
+}
+
+func TestDoMath(t *testing.T) {
+	got := doMath(7, 5, add)
+	want := 12
+	if got != want {
+		t.Errorf("Sum was incorrect, got: %d, want: %d.", got, want)
+	}
+}
