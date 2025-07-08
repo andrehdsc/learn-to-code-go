6f740dc7f70517e285d781d98051939a92e7f5eb
andrehdsc <andrehdsc@gmail.com>
2025-07-08 16:12:44 -0300

    HOE-102

--- 255-HOE-102/word/main_test.go
+++ 255-HOE-102/word/main_test.go
@@ -1,42 +1,38 @@
-package main
+package word
 
 import (
 	"fmt"
 	"testing"
-
-	"github.com/andrehdsc/learn-to-code-go/255-HOE-102/word"
 )
 
 func BenchmarkCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		word.Count("one Two three")
+		Count("one Two three")
 	}
 }
 
 func BenchmarkUseCount(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		word.UseCount("one Two three")
+		UseCount("one Two three")
 	}
 }
 
 func ExampleCount() {
-	fmt.Println(word.Count("one two tree"))
+	fmt.Println(Count("one two tree"))
 	//Output:
 	//3
 }
 
 func TestCount(t *testing.T) {
-	wc := word.Count("one two three")
+	wc := Count("one two three")
 	if wc != 3 {
 		t.Errorf("got: %v, want: 3\n", wc)
 	}
 }
 
 func TestUseCount(t *testing.T) {
-	wc := word.UseCount("one one one")
+	wc := UseCount("one one one")
 	if wc["one"] != 3 {
 		t.Errorf("got: %v, want: 3\n", wc["one"])
 	}
 }
-
-
